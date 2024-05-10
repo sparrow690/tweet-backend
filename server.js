23df017b973d63ee6a7cbd0600fa6ef3a6c6486d
landry kassius <lkassindi@gmail.com>
2024-05-10 09:26:32 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import data from './data.js';
+
+
+const Port = 3000
+
+const app = express();
+let mesTweets = [...data.tweets]
+
+app.use(express.json());
+app.get('/tweets', (req, res) => {
+    res.send(mesTweets);
+})
+
+app.post('/tweets', (req, res) => {
+    const newTweets = req.body
+    mesTweets.push(newTweets);
+    res.send(newTweetsTweets);
+})
+
+app.get('user/:handler', (req, res) => {
+    const theuser = req.params.handler;
+    const userTweets = mesTweets.filter((tweet) => tweet.user.username === theuser)
+
+    res.send(userTweets);
+})
+
+app.get('/user', (req, res) => {
+    res.send(data["current-user"]);
+})
+
+ app.listen(Port);
+
